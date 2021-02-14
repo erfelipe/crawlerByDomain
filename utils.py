8129fa474879449eb2a8635b089d9244d0bc41ca
Eduardo <erfelipe@yahoo.com.br>
2021-02-14 18:53:04 -0300

    Versão com funcionalidades básicas

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
-from sqlite3.dbapi2 import Cursor
+# from sqlite3.dbapi2 import Cursor
+from urllib.parse import urlparse
 import crawDomain as craw 
 import consts
 import sqlite3
@@ -14,8 +15,9 @@ def fillKeywords():
         keywords.append(l)
 
 def fillQueueWithSeeds(queueUrlsVisitar):
-    queueUrlsVisitar.put("www.teste.com.br")
-    queueUrlsVisitar.put("www.outro.com.br")
+    seeds = loadFileLikeArray(consts.ARQ_SEEDS)
+    for seed in seeds:
+        queueUrlsVisitar.put(seed)
 
 def saveQueueToVisit():
     pass
@@ -25,18 +27,19 @@ def createDataBase():
     cursor = conn.cursor() 
     cursor.execute("""  CREATE TABLE if not exists crawlerByDomain(
                         id integer primary key autoincrement,
-                        url text not null
-                        page text not null
-                        keys text not null
+                        url text not null,
+                        page text not null,
+                        keys text not null )
                     """)
     conn.commit()
     conn.close() 
 
 def insertDataInDB(url, page, keywordsFound):
+    keys = ', '.join(keywordsFound)
     conn = sqlite3.connect(consts.ARQ_DATABASE) 
     cursor = conn.cursor() 
     cursor.execute("""  INSERT INTO crawlerByDomain (url, page, keys) 
-                        VALUES (?, ?, ? )""", (url, page, keywordsFound, ))
+                        VALUES (?, ?, ? )""", (url, page, keys, ))
     conn.commit()
     conn.close() 
 
@@ -45,13 +48,13 @@ def initialize(queueUrlsVisitar):
     fillKeywords()
     fillQueueWithSeeds(queueUrlsVisitar)
 
-def textoSemAcentos(texto):
+def textNoAccent(texto):
     text = unicodedata.normalize('NFD', texto)\
            .encode('ascii', 'ignore')\
            .decode("utf-8")
     return text
 
-def carregarArquivoComoArray(arq):
+def loadFileLikeArray(arq):
     """ Le o arquivo txt e gera um array em minusculas e sem espaços nas bordas
         Muito importante notar que caso tenha algum acento, o texto equivalente sem acento sera adicionado aa lista
     Returns:
@@ -62,6 +65,12 @@ def carregarArquivoComoArray(arq):
     for item in listaMinuscula:
         for letra in caracteresAcentuados:
             if letra in item:
-                listaMinuscula.append(textoSemAcentos(item.lower()))
+                listaMinuscula.append(textNoAccent(item.lower()))
     return set(listaMinuscula)
 
+def urlWellFormat(url):
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc, result.path])
+    except:
+        return False
