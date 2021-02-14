4819f03df6e03f94a462032ae377bf5193a5847b
Eduardo <erfelipe@yahoo.com.br>
2021-02-14 17:27:32 -0300

    Versão alfa

---
+++ crawDomain.py
@@ -0,0 +1,73 @@
+import queue
+import utils
+import requests
+import consts
+from urlextract import URLExtract 
+
+begin = 0
+limiteVisitas = 9000 
+keywordsList = []
+queueUrlsToVisit = queue.Queue() 
+queueUrlsVisited = []
+
+def urlNotVisited(url):
+    return url not in queueUrlsVisited
+
+def contentFromUrl(url):
+    data = requests.get(url)
+    if (data.status_code == requests.codes.ok):
+        return data.text
+    else:
+        return ""
+
+def keyswordsInDocument(data):
+    keys = []
+    for k in keywordsList:
+        if k in data:
+            keys.append(k)
+    return keys
+
+def allUrlsFromDocument(data):
+    extractor = URLExtract()
+    urls = extractor.find_urls(data)
+    return urls
+
+def addUrlsToVisit(urls):
+    for url in urls:
+        queueUrlsToVisit.put(url)
+
+def processUrl(url):
+    """ For each url, process it
+
+    Args:
+        url (str): internet address 
+    """ 
+    allUrls = []
+    keywordsFound = []
+    if (urlNotVisited(url)):
+        page = contentFromUrl(url)
+        keywordsFound = (keyswordsInDocument(page))
+        if (len(keywordsFound) > 0): 
+            allUrls = allUrlsFromDocument(page)
+            utils.insertDataInDB(url, page, keywordsFound)
+    # se tiver keywords:
+    # - lista todas as urls e cadastra na fila a visitar
+    # - armazena: url, conteudo, quais keyswords foram encontradas
+    # coloca endereco na fila de visitadas
+    else: 
+        print(url, " já visitada.")
+    queueUrlsVisited.append(url)
+    addUrlsToVisit(allUrls)
+
+def processQueue(begin):
+    """ Process the queue with seeds and prior address
+    """    
+    while (begin < limiteVisitas) and (not queueUrlsToVisit.empty()):
+        begin += 1
+        url = queueUrlsToVisit.get()
+        processUrl(url) 
+
+if __name__ == "__main__":
+    utils.initialize(queueUrlsToVisit)
+    keywordsList = utils.carregarArquivoComoArray(consts.ARQ_KEYWORDS)
+    processQueue(begin)
