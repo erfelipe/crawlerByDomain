80153d32dec6fce8988f5e94bffa8e77634aec71
Eduardo Felipe <erfelipe@yahoo.com.br>
2021-12-17 21:32:02 -0300

    Search for select seeds

--- searchFromGoogle-2.py
+++ searchFromGoogle-2.py
@@ -1,14 +1,15 @@
 import utils
 import time
-from googlesearch import search
+from serpapi import GoogleSearch
+import json
 
-def searchWeb():
+def searchWeb(): 
     terms = listTermsForQueries()
     resultSearchGlobal = []
     userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36 Edg/95.0.1020.40'
     cont = 0
     for term in terms:
-        # A cada 6 termos, o programa espera 1 minuto para prosseguir
+        # A cada n termos, o programa espera 1 minuto para prosseguir
         cont = cont + 1
         if ((cont % 4) == 0):
             print("## Aguardando 1 min ##")
@@ -16,12 +17,20 @@ def searchWeb():
         resultSearchByTerm = []
         saveUrls(["#######",term, "#######"])
         print("####### " ,term, " #######")
-        for t in search(term, 150, lang="es"):
-            print(t)
-            if (t not in resultSearchGlobal):
-                resultSearchByTerm.append(t)
-                resultSearchGlobal.append(t)
-        saveUrls(resultSearchByTerm)
+        results = GoogleSearch({
+            "q": term,
+            "location": "Region of Murcia, Spain",
+            "api_key": "776b0f838bb3bc24924749ff72537cc21945a7cfd31cb0fd64029919165d2751"}).get_dict()
+        # print(results)
+        print(json.dumps(results, indent=4))
+
+        
+        # for item in results:
+        #     print(item.link)
+        #     if (item.link not in resultSearchGlobal):
+        #         resultSearchByTerm.append(item.link)
+        #         resultSearchGlobal.append(item.link)
+        # saveUrls(resultSearchByTerm)
         # A cada pesquisa, aguardar 15 segundos
         print("## Aguardando 15 seg ##")
         time.sleep(15)
@@ -32,10 +41,9 @@ def saveUrls(urls):
 
 def listTermsForQueries():
     # terms = ["Homeopatía", "Nosode",  "Samuel Hahnemann", "Industria homeopática Boiron", "Industria homeopática COFENAT", "Industria homeopática Homeopathy Research Institute", "Industria homeopática Observatorio de Terapias Naturales", "Industria homeopática Swiss Association of Homeopathic Physicians", "Industria homeopática SAHOP/SVHA", "Miasmas", "Ley de los similares", "Ley de los infinitesimales", "Agua polimerizada", "Memoria del agua", "Dilución homeopática", "Número de Avogadro", "Constante de Avogadro", "Prueba homeopática", "Repertorios homeopáticos", "Homeopathic Materia Medica", "Productos homeopáticos", "Tratamientos homeopáticos", "Medicina homeopática", "Remedios homeopáticos", "Homeópata"]
-    
-    # indicar quais os termos devem ser procurados, para evitar o erro 429, 4-5 termos por vez funcionam
+    # indicar quais os termos devem ser procurados, para evitar o erro 429, 8 termos por vez funcionam
     terms = ["Homeopatía", "Nosode",  "Samuel Hahnemann", "Industria homeopática Boiron", "Industria homeopática COFENAT", "Industria homeopática Homeopathy Research Institute", "Industria homeopática Observatorio de Terapias Naturales", "Industria homeopática Swiss Association of Homeopathic Physicians", "Industria homeopática SAHOP/SVHA", "Miasmas", "Ley de los similares", "Ley de los infinitesimales", "Agua polimerizada", "Memoria del agua", "Dilución homeopática", "Número de Avogadro", "Constante de Avogadro", "Prueba homeopática", "Repertorios homeopáticos", "Homeopathic Materia Medica", "Productos homeopáticos", "Tratamientos homeopáticos", "Medicina homeopática", "Remedios homeopáticos", "Homeópata"]
-    return terms
+    return ["Homeopatía"]
 
 if __name__ == "__main__":
     searchWeb()
